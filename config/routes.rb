d471d027df54eba3b8261bd9fa67eb8ccf3d8fb5
Kazuki <edak1209@gmail.com>
2018-11-30 13:17:46 +0900

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   devise_for :users, skip: :all
   devise_scope :user do
     get 'login', to: 'users/sessions#new', as: :new_user_session
-    post 'login', to: 'users/sessions#create'
-    get 'signup', to: 'users/registrations#index', as: :index_user_registration
+    post 'registrations', to: 'users/sessions#create'
+    delete 'signup', to: 'users/sessions#destroy', as: :destroy_session
     get 'signup/registration', to: 'users/registrations#new', as: :new_user_registration
     post 'signup/registration', to: 'users/registrations#create'
-    delete 'signup', to: 'users/sessions#destroy', as: :destroy_session
   end
 
   resources :registrations
@@ -25,7 +24,6 @@ Rails.application.routes.draw do
       get 'get_category_ss'
       get 'deal'
       get 'completion'
-      # /profiles/get_cities
     end
   end
 
