f419fe08fcf54c6071138c7c1054a7898d4a50d1
Kazuki <edak1209@gmail.com>
2018-11-30 14:59:38 +0900

    Modify routes

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,12 +7,6 @@ crumb :user_mypage do
  parent :root
 end
 
- # #user/mypage/logout
- # crumb :user_logout do
- #   link "ログアウト",root_mypage
- #   parent :mypage
- # end
-
  #item/show
 crumb :item_show do
  link "コーチなんだよ！！！！！！！！！！！！！！！！！！",items_path
